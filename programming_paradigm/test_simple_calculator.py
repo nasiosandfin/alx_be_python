92960f17b3f9a83d2f2306dd43d344983d4ae523
Sandfin Nasio <nasiosandfin@gmail.com>
2025-11-30 21:18:53 +0300

    Add unit tests for SimpleCalculator class covering add, subtract, multiply, divide

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,10 +13,11 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(10, 4), 6)
         self.assertEqual(self.calc.subtract(-5, -2), -3)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(-2, 5), -10)
 
     def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertIsNone(self.calc.divide(10, 0))
+
