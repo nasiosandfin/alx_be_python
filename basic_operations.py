b957b2c2943dd46bde811c9a38a5c194e37c27c7
Sandfin Nasio <nasiosandfin@gmail.com>
2025-11-30 17:21:15 +0300

    Add stashed files and directories

---
+++ basic_operations.py
@@ -0,0 +1,17 @@
+
+# basic_operations.py
+
+# Define the numbers
+number1 = 10
+number2 = 5
+
+# Perform arithmetic operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print the results
+print(number1 + number2)
+print(number1 - number2)
+print(number1 * number2)
+
